2262fe29411c6a1fca362532d137567de17f97f8
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-11 14:35:19 -0500

    Got regular expressions working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,97 +1,81 @@
 const friends = [
     {
-        "name":"Obiwan Kenobi",
-        "photo":"http://vignette4.wikia.nocookie.net/starwars/images/4/4e/ObiWanHS-SWE.jpg/revision/latest?cb=20111115052816",
+        "name":"Charlie Brown",
+        "photo":"https://static.comicvine.com/uploads/square_small/2/26320/487898-charliebrown.png",
         "scores":[
+            2,
+            1,
+            4,
             5,
-            3,
-            4,
-            4,
-            3,
-            4,
+            2,
             2,
             3,
-            4,
-            2
-          ]
-      },
-      {
-        "name":"Qui-Gon Jinn",
-        "photo":"http://vignette1.wikia.nocookie.net/swfanon/images/d/d6/QuiGonPortrait-SWE.jpg/revision/latest?cb=20130205204403",
-        "scores":[
-            4,
             3,
-            2,
-            5,
-            5,
-            4,
-            2,
-            5,
             4,
             3
           ]
       },
       {
-        "name":"Yoda",
-        "photo":"http://1.bp.blogspot.com/_bFvPrx9a3CU/Ry5_eSEjofI/AAAAAAAAACY/E3HFdBll8jo/s320/maestro_yoda_small.jpg",
+        "name":"Lucy Van Pelt",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/e/e9/Lucy_van_Pelt.png",
+        "scores":[
+            3,
+            4,
+            3,
+            3,
+            1,
+            3,
+            5,
+            2,
+            2,
+            2
+          ]
+      },
+      {
+        "name":"Snoopy",
+        "photo":"https://images-na.ssl-images-amazon.com/images/I/31QYmH32t0L._SL500_AC_SS350_.jpg",
         "scores":[
             5,
-            5,
-            4,
-            5,
-            5,
-            1,
             2,
             5,
-            4,
+            3,
+            2,
+            5,
+            3,
+            5,
+            1,
             5
           ]
       },
       {
-        "name":"Darth Sidious",
-        "photo":"http://vignette3.wikia.nocookie.net/villains/images/2/24/Tumblr_md888pURhc1rkgyw3o3_1280.jpg/revision/latest/scale-to-width-down/183?cb=20131104131154",
+        "name":"Pig Pen",
+        "photo":"https://vignette.wikia.nocookie.net/peanuts/images/b/bd/Pigpen.jpg/revision/latest?cb=20160715070328",
         "scores":[
-            2,
-            1,
             3,
-            4,
-            2,
-            1,
-            2,
+            3,
+            3,
+            3,
             5,
+            3,
             2,
+            2,
+            3,
             1
           ]
       },
       {
-        "name":"Darth Maul",
-        "photo":"http://vignette4.wikia.nocookie.net/starwars/images/5/50/Darth_Maul_profile.png/revision/latest?cb=20140209162228",
+        "name":"Linus Van Pelt",
+        "photo":"https://upload.wikimedia.org/wikipedia/en/5/5e/Linus_van_Pelt.gif",
         "scores":[
-            2,
+            4,
             1,
             4,
-            4,
-            2,
-            1,
-            2,
-            4,
-            4,
-            1
-          ]
-      },
-      {
-        "name":"Darth Vader",
-        "photo":"https://www.sideshowtoy.com/wp-content/uploads/2015/03/star-wars-darth-vader-lord-of-the-sith-premium-format-thumb-300093-175x175.jpg",
-        "scores":[
-            3,
-            1,
-            3,
-            3,
             5,
-            1,
             2,
-            5,
+            2,
+            2,
             4,
+            2,
             3
           ]
       }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,25 +12,28 @@ module.exports = function (app) {
     // Recieves JSON POST survey results
     app.post("/api/friends", function (req, res) {
         const survey = req.body;
-        let results = [];
+        let sortedResults = [];
 
         let getDiff = () => {
             // loop for each friend
             let score = 0;
             // loop through each friend
             let temp = friends.map(element => {
-                console.log (element.name, element.scores);
+                console.log (element.scores, element.name);
+                console.log (survey.scores, survey.name);
                 let tmpArray = [];
                 for (let i=0; i < element.scores.length; i++) {
-                    tmpArray.push(Math.abs(element.scores[i] - survey.scores[i]));
-                    console.log(`${element.scores[i]}- ${survey.scores[i]}`);
+                    tmpArray.push(Math.abs(element.scores[i] - parseInt(survey.scores[i])));
+                    // console.log(`${element.scores[i]}- ${survey.scores[i]}`);
                 }
+                console.log(tmpArray);
                 // sum of array from https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
                 let obj = {
                     name: element.name,
                     photo: element.photo,
                     score: tmpArray.reduce((partial_sum, a) => partial_sum + a)
                 }
+                console.log(`Difference: ${obj.score}`);
                 return obj;
                 
             })
@@ -38,15 +41,15 @@ module.exports = function (app) {
             console.log('Sorted:');
             // Sorted by lowest from .sort() method http://www.javascriptkit.com/javatutors/arraysort2.shtml
             // console.log(temp.sort( (a,b) => {return a.score - b.score} ) );
-            results = temp.sort( (a,b) => {return a.score - b.score} );
-            console.log(results);
+            sortedResults = temp.sort( (a,b) => {return a.score - b.score} );
+            console.log(sortedResults);
         }
 
         console.log(req.body);
         getDiff();
         // Display the JSON to the client
         // res.json({result: results[0]});
-        res.json(results[0]); // return first element of sorted array
+        res.json(sortedResults[0]); // return first element of sorted array
     });
 
 }
