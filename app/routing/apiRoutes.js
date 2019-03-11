364a018e731018f5eee0823b7e925fc6ba72cf4b
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-11 13:07:48 -0500

    Test logic and find example friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
     // Recieves JSON POST survey results
     app.post("/api/friends", function (req, res) {
         const survey = req.body;
-        let smallest = false;
+        let results = [];
 
         let getDiff = () => {
             // loop for each friend
@@ -28,21 +28,25 @@ module.exports = function (app) {
                 // sum of array from https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
                 let obj = {
                     name: element.name,
+                    photo: element.photo,
                     score: tmpArray.reduce((partial_sum, a) => partial_sum + a)
                 }
                 return obj;
                 
             })
-            console.log(temp);
+            // console.log(temp);
             console.log('Sorted:');
-            // sort method http://www.javascriptkit.com/javatutors/arraysort2.shtml
-            console.log(temp.sort( (a,b) => {return a.score - b.score} ) );
+            // Sorted by lowest from .sort() method http://www.javascriptkit.com/javatutors/arraysort2.shtml
+            // console.log(temp.sort( (a,b) => {return a.score - b.score} ) );
+            results = temp.sort( (a,b) => {return a.score - b.score} );
+            console.log(results);
         }
 
         console.log(req.body);
         getDiff();
         // Display the JSON to the client
-        res.json(req.body);
+        // res.json({result: results[0]});
+        res.json(results[0]); // return first element of sorted array
     });
 
 }
