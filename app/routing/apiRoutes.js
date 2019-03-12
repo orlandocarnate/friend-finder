c7712ecada6a75c5741821215cb28d044d715c29
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-12 00:21:13 -0500

    Added latest member to front page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,12 @@ module.exports = function (app) {
         return res.json(friends); // RETURN JSON
     });
 
+    // Get the last item from the friends list to show
+    app.get("/api/latest", function (req, res) {
+        // return JSON data
+        return res.json(friends[friends.length - 1]); // RETURN last item from object array
+    });
+
     // Recieves JSON POST survey results
     app.post("/api/friends", function (req, res) {
         const survey = req.body;
@@ -20,16 +26,16 @@ module.exports = function (app) {
         }
         // add new friend to friends array of objects
         let sortedResults = [];
-        
+
         let getDiff = () => {
             // loop for each friend
             let score = 0;
             // loop through each friend
             let temp = friends.map(element => {
-                console.log (element.scores, element.name);
-                console.log (newFriend.scores, newFriend.name);
+                console.log(element.scores, element.name);
+                console.log(newFriend.scores, newFriend.name);
                 let tmpArray = [];
-                for (let i=0; i < element.scores.length; i++) {
+                for (let i = 0; i < element.scores.length; i++) {
                     tmpArray.push(Math.abs(element.scores[i] - newFriend.scores[i]));
                 }
                 console.log(tmpArray);
@@ -41,13 +47,13 @@ module.exports = function (app) {
                 }
                 console.log(`Difference: ${obj.score}`);
                 return obj;
-                
+
             })
             // console.log(temp);
             console.log('Sorted:');
             // Sorted by lowest from .sort() method http://www.javascriptkit.com/javatutors/arraysort2.shtml
             // console.log(temp.sort( (a,b) => {return a.score - b.score} ) );
-            sortedResults = temp.sort( (a,b) => {return a.score - b.score} );
+            sortedResults = temp.sort((a, b) => { return a.score - b.score });
             console.log(sortedResults);
             friends.push(newFriend);
             console.log("Friends Array: ", friends);
