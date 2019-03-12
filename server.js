8ced8eeb99dbe969e1dc464095c8867fc7dda0f6
Orlando Carnate <orlando.carnate@gmail.com>
2019-03-11 22:57:04 -0500

    Set port to deploy

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Express routes
 // =============================================================
